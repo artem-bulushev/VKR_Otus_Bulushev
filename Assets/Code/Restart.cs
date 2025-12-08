80b76fa26d6bfdcb6eb4ab83498f09fd503979f3
artem-bulushev <bulushiev93@mail.ru>
2025-12-08 11:31:17 +0300

    Изменения: - создан рестарт уровня при падении на землю

---
+++ Assets/Code/Restart.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace Code
+{
+    public class Restart : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out FirstPersonController player))
+            {
+                RestartLavel();
+                Debug.Log("Уровень перезапущен");
+            }
+        }
+        public void RestartLavel()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+        }
+    }
+}
